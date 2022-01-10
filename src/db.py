b68e31b4e8820759bea6f6705d27abec6b520188
Thomas Sirack <t.sirack@share.epsb.ca>
2022-01-10 13:40:11 -0700

    create db.py

---
+++ src/db.py
@@ -0,0 +1,15 @@
+"""
+db.py
+
+Title: SQL database functions
+Author: Thomas Sirack
+Date: 2022-01-10
+"""
+
+"""
+This file deals with the creation and management of the database.
+"""
+
+### "__main__ escape" ###
+if __name__ = "__main__":
+  pass
