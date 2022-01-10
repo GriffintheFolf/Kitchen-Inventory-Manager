e655f3ce680c18e3a031fbb727b47313fd91507c
Thomas Sirack <t.sirack@share.epsb.ca>
2022-01-10 12:35:53 -0700

    reorganize source code layout

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+main.py
+
+Title: main entry point for program
+Author: Thomas Sirack
+Date: 2022-01-10
+"""
+
+if __name__ == "__main__":
+  pass
---
+++ src/flask.py
@@ -0,0 +1,41 @@
+"""
+flask.py
+
+Title: Flask server
+Author: Thomas Sirack
+Date created: 2021-12-17
+"""
+
+"""
+This file mostly deals with creating the Flask server and serving
+the pages.
+"""
+
+from flask import Flask, render_template, request
+
+### Flask-related variables ##
+APPLICATION = Flask(__name__)
+
+## page routing ##
+@APPLICATION.route("/")
+
+## page definitions ##
+def index():
+  """
+  The main page of the website (index.html).
+  """
+
+  return render_template("index.html")
+
+### subroutines ###
+def start_flask():
+  """
+  Starts the Flask server.
+  """
+
+  global APPLICATION
+  APPLICATION.run(debug=True)
+
+### "__main__ escape" ###
+if __name__ == "__main__":
+  pass
